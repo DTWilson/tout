2058682d8c59241a7d60bb9062d6982a70e56174
DTWilson <d.t.wilson@leeds.ac.uk>
2023-01-10 09:44:36 +0000

    Improved binary PC optimisation

--- R/opt_pc.R
+++ R/opt_pc.R
@@ -30,15 +30,22 @@
 opt_pc <- function(n, rho_0, rho_1, alpha_nom, beta_nom, gamma_nom){
   
   x_1s <- 0:n
-  beta <- local_beta(x_1s, n, rho_0, rho_1, alpha_nom)
+  
+  # For each possible x_1, find the x_0 which gives "exact" control of alpha.
+  x_0s <- opt_x_0(x_1s, n, rho_0, alpha_nom)
+  
+  # Get cosponsoring error rates and extract beta
+  ocs <- get_ocs(n, x_0s, x_1s, rho_0, rho_1)
+  beta <- ocs[,2]
+  
+  # Convert this beta into an objective to minimise, penalising any beta
+  # constraint violations and any cases where x_0 > x_1
   beta2 <- abs(beta - beta_nom)
-  beta3 <- beta2 + (beta > beta_nom)*10
+  beta3 <- beta2 + (beta > beta_nom)*10 + (x_0s > x_1s)*10
   
+  # Use this objective to determine the best x_1
   x_1 <- x_1s[which.min(beta3)]
-  
-  # Determine the corresponding x_0 to give alpha = alpha_nom
-  z <- 2 - 2*alpha_nom - stats::pbinom(x_1, n, rho_0)
-  x_0 <- stats::qbinom(z, n, rho_0)
+  x_0 <- x_0s[which.min(beta3)]
   
   # Get operating characteristics and check if gamma_nom is satisfied
   ocs <- get_ocs(n, x_0, x_1, rho_0, rho_1)
@@ -52,11 +59,9 @@ opt_pc <- function(n, rho_0, rho_1, alpha_nom, beta_nom, gamma_nom){
   return(design)
 }
 
-local_beta <- function(x_1, n, rho_0, rho_1, alpha_nom){
+opt_x_0 <- function(x_1, n, rho_0, alpha_nom){
   # For given x_1 find the x_0 which best satisfies alpha_nom
-  # and return corresponding beta
   z <- 2 - 2*alpha_nom - stats::pbinom(x_1, n, rho_0)
   x_0 <- stats::qbinom(z, n, rho_0)
-  ocs <- get_ocs(n, x_0, x_1, rho_0, rho_1)
-  return(ocs[,2] + (x_0 > x_1)*10)
+  return(x_0)
 }
