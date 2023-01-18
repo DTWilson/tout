19bff0cd7bc71f75e5fe98bd7e62b9e607994434
DTWilson <d.t.wilson@leeds.ac.uk>
2023-01-18 10:10:42 +0000

    Corrected adjustm ent interval implementation (continuous)

--- R/opt_pc_adjust_cont.R
+++ R/opt_pc_adjust_cont.R
@@ -41,13 +41,8 @@ opt_pc_adjust_cont <- function(n, rho_0, rho_1, sigma, alpha_nom, beta_nom, tau_
   x_1 <- stats::qnorm(1 - alpha_nom)
   
   # Find x_0 such that alpha_2 = alpha_nom
-  # Need a principled way of choosing the minimum for the search.
-  x_0_min <- -10
-  # Now search numerically:
-  x_0 <- stats::optimise(get_alpha_2_cont, lower = x_0_min, upper = x_1,
-                  x_1=x_1, alpha_nom=alpha_nom, sigma=sigma, n=n, 
-                  tau_min=tau_min, tau_max=tau_max)$minimum
-  
+  x_0 <- stats::qnorm(1 - alpha_nom, mean = (rho_0 - tau_min)*sqrt(n)/sigma)
+
   # Find beta_1, which will be maximised at tau_max distance from rho_1
   beta <- stats::pnorm(x_0, mean = ((rho_1 - rho_0) - tau_max)*sqrt(n)/sigma)
   
@@ -60,19 +55,4 @@ opt_pc_adjust_cont <- function(n, rho_0, rho_1, sigma, alpha_nom, beta_nom, tau_
   }
   
   return(c(design, ocs))
-}
-
-get_alpha_2_cont <- function(x_0, x_1, alpha_nom, sigma, n, tau_min, tau_max){
-  
-  # Get mid point of the adjustment interval
-  mid_point <- (x_0 + x_1)/2
-  
-  # alpha_2 will be maximised at this mid point, or as close to it as 
-  # allowed by the range of tau.
-  tau_ncp <- max(min(mid_point, -tau_min*sqrt(n)/sigma), -tau_max*sqrt(n)/sigma)
-  
-  alpha_2 <- stats::pnorm(x_1, mean = tau_ncp) - stats::pnorm(x_0, mean = tau_ncp)
-  
-  # Return squared difference to minimise
-  return((alpha_2 - alpha_nom)^2)
 }
