46ab6fbc355676895fc47c9a258377ed7221202f
Duncan T. Wilson <meddwilb@leeds.ac.uk>
2024-07-12 11:56:44 +0100

    5 use base r for plots (#8)

--- R/tout.R
+++ R/tout.R
@@ -32,32 +32,118 @@ print.tout <- function(x, ...){
 #' @importFrom ggplot2 aes
 #' @export
 plot.tout <- function(x, y, ...){
-  # Vector of possible outcomes
-  ys <- 0:x$n
-  # Probabilities of these outcomes under the null and alternative hypotheses
-  probs_null <- stats::dbinom(ys, size = x$n, prob = x$hyps[1])
-  probs_alt <- stats::dbinom(ys, size = x$n, prob = x$hyps[2])
   
-  # Data frame for plotting, flagging all direct and indirect errors
-  df <- data.frame(y = ys,
-                   p_n = probs_null,
-                   tI = c(rep("None", x$thresholds[1] + 1), 
-                          rep("Indirect type I", x$thresholds[2] - x$thresholds[1]), 
-                          rep("Direct type I", x$n - x$thresholds[2])),
-                   p_a = probs_alt,
-                   tII = c(rep("Direct type II", x$thresholds[1] + 1), 
-                           rep("Indirect type II", x$thresholds[2] - x$thresholds[1]), 
-                           rep("None", x$n - x$thresholds[2])))
+  if(is.null(x$sigma)){
+    # Binary case
+    # Vector of possible outcomes
+    ys <- 0:x$n
+    # Probabilities of these outcomes under the null and alternative hypotheses
+    probs_null <- stats::dbinom(ys, size = x$n, prob = x$hyps[1])
+    probs_alt <- stats::dbinom(ys, size = x$n, prob = x$hyps[2])
+    
+    # Data frame for plotting, flagging all direct and indirect errors
+    df <- data.frame(y = ys,
+                     p_n = probs_null,
+                     tI = c(rep("None", x$thresholds[1] + 1), 
+                            rep("Indirect type I", x$thresholds[2] - x$thresholds[1]), 
+                            rep("Direct type I", x$n - x$thresholds[2])),
+                     p_a = probs_alt,
+                     tII = c(rep("Direct type II", x$thresholds[1] + 1), 
+                             rep("Indirect type II", x$thresholds[2] - x$thresholds[1]), 
+                             rep("None", x$n - x$thresholds[2])))
+    
+    df <- df[df$p_n > 0.00001 | df$p_a > 0.00001,]
+    
+    barplot(names=df$y, height=df$p_n,
+            col=ifelse(df$tI == "Direct type I", t_col("darkgreen"),
+                       ifelse(df$tI == "Indirect type I", t_col("orange"), t_col("gray70", 100))),
+            main = expression(paste("Sampling distributions under null (", rho[0], ") and alternative (", rho[1], ") hypotheses")))
+    par(new = TRUE)
+    barplot(names=df$y, height=df$p_a,  yaxt = "n",
+            col=ifelse(df$tII == "Direct type II", t_col("red"),
+                       ifelse(df$tII == "Indirect type II", t_col("orange"), t_col("gray70", 100))))
+    legend("topright", 
+           legend = c("Direct type I", "Direct type II", "Pause"), 
+           col = c(t_col("darkgreen"), t_col("red"), t_col("orange")),
+           pch = 15,
+           bty = "n", 
+           pt.cex = 2, 
+           cex = 1.2, 
+           text.col = "black", 
+           horiz = F , 
+           inset = c(0.0, 0.0))
+    
+  } else {
+    # Continuous case
+    ncp <- sqrt(x$n)*(x$hyps[2] - x$hyps[1])/x$sigma
+    # Vector of possible outcomes
+    ys <- seq(-3, ncp + 3, 0.01)
+    # Probabilities of these outcomes under the null and alternative hypotheses
+    probs_null <- stats::dnorm(ys, mean = 0, sd = 1)
+    probs_alt <- stats::dnorm(ys, mean = ncp, 1)
+    
+    # Data frame for plotting, flagging all direct and indirect errors
+    df <- data.frame(y = ys,
+                     p_n = probs_null,
+                     p_a = probs_alt)
+    df$tI <- ifelse(df$y <= x$thresholds[1], "None",
+                    ifelse(df$y >= x$thresholds[2], "Direct type I", "Indirect type I"))
+    df$tII <- ifelse(df$y >= x$thresholds[2], "None",
+                    ifelse(df$y <= x$thresholds[1], "Direct type II", "Indirect type II"))
+    
+    df <- df[df$p_n > 0.00001 | df$p_a > 0.00001,]
+    
+    plot(df$y, df$p_n, type = "l",  lwd=1, ylab='', xlab='',
+         main = expression(paste("Sampling distributions under null (", rho[0], ") and alternative (", rho[1], ") hypotheses")))
+    
+    polygon(x = c(df$y[df$tI == "Direct type I"], max(df$y), x$thresholds[2], x$thresholds[2]),
+            y = c(df$p_n[df$tI == "Direct type I"], 0, 0, stats::dnorm(x$thresholds[2], mean = 0, sd = 1)),
+            col = t_col("darkgreen"), lty=0)
+    
+    polygon(x = c(df$y[df$tI == "Indirect type I"], x$thresholds[2], x$thresholds[1], x$thresholds[1]),
+            y = c(df$p_n[df$tI == "Indirect type I"], 0, 0, stats::dnorm(x$thresholds[1], mean = 0, sd = 1)),
+            col = t_col("orange"), lty=0)
+
+    par(new = TRUE)
+    
+    plot(df$y, df$p_a, type = "l", lwd=1, ylab='', xlab='')
+    
+    polygon(x = c(min(df$y), df$y[df$tII == "Direct type II"], x$thresholds[1], x$thresholds[1]),
+            y = c(0, df$p_a[df$tII == "Direct type II"], stats::dnorm(x$thresholds[1], mean = ncp, sd = 1), 0),
+            col = t_col("red"), lty=0)
+    
+    polygon(x = c(x$thresholds[1], df$y[df$tII == "Indirect type II"], x$thresholds[2], x$thresholds[2], x$thresholds[1]),
+            y = c(stats::dnorm(x$thresholds[1], mean = ncp, sd = 1), df$p_a[df$tII == "Indirect type II"], 
+                  stats::dnorm(x$thresholds[2], mean = ncp, sd = 1), 0, 0),
+            col = t_col("orange"), lty=0)
+
+    legend("topright", 
+           legend = c("Direct type I", "Direct type II", "Pause"), 
+           col = c(t_col("darkgreen"), t_col("red"), t_col("orange")),
+           pch = 15,
+           bty = "n", 
+           pt.cex = 2, 
+           cex = 1.2, 
+           text.col = "black", 
+           horiz = F , 
+           inset = c(0.0, 0.0))
+  }
+}
+
+t_col <- function(color, percent = 50, name = NULL) {
+  #      color = color name
+  #    percent = % transparency
+  #       name = an optional name for the color
   
-  ggplot2::ggplot(df, aes(x = y)) + 
-    ggplot2::geom_bar(aes(y = p_n, fill = tI, colour = tI), stat = "identity", alpha = 0.4) + 
-    ggplot2::geom_bar(aes(y = probs_alt, fill = tII, colour = tII), stat = "identity", alpha = 0.4) +
-    ggplot2::scale_fill_manual(values = c("red", "darkgreen", "orange",  "green", "gray70"), 
-                               name = "Error") +
-    ggplot2::scale_colour_manual(values = c("red", "darkgreen", "orange",  "green", "gray70"), 
-                                 name = "Error") +
-    ggplot2::geom_vline(xintercept = x$thresholds[1], linetype = 2) +
-    ggplot2::geom_vline(xintercept = x$thresholds[2], linetype = 2) +
-    ggplot2::ylab("Probability") + ggplot2::xlab("Outcome") + 
-    ggplot2::theme_minimal()
+  ## Get RGB values for named color
+  rgb.val <- col2rgb(color)
+  
+  ## Make new color using input color as base and alpha set by transparency
+  t.col <- rgb(rgb.val[1], rgb.val[2], rgb.val[3],
+               max = 255,
+               alpha = (100 - percent) * 255 / 100,
+               names = name)
+  
+  ## Save the color
+  invisible(t.col)
 }
