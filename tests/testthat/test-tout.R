22c1cca38f7704360d0d26c54c4c7d967c6c0bb4
Duncan Wilson <meddwilb@leeds.ac.uk>
2025-10-08 09:56:05 +0100

    Fixed plot unit tests

--- tests/testthat/test-tout.R
+++ tests/testthat/test-tout.R
@@ -11,11 +11,13 @@ test_that("printing works (continuous)", {
 
 test_that("plotting works (binary)", {
   x <- tout_design(rho_0=0.5, rho_1=0.7, alpha_nom=0.05, beta_nom=0.1, gamma_nom = 0.9)
-  vdiffr::expect_doppelganger("Binary tout plot", x)
+  bin_fig <- function() plot(x)
+  vdiffr::expect_doppelganger("Binary tout plot", bin_fig)
 })
 
 test_that("plotting works (continuous)", {
   x <- tout_design(rho_0=0, rho_1=0.4, alpha_nom=0.05, beta_nom=0.1, gamma_nom = 0.6, sigma=1)
-  vdiffr::expect_doppelganger("Continuous tout plot", x)
+  cont_fig <- function() plot(x)
+  vdiffr::expect_doppelganger("Continuous tout plot", cont_fig)
 })
 
